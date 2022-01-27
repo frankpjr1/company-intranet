22e53485e24c0767760d45b050a5a64d9ff8cb1b
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-01-26 20:15:57 -0500

    commit created logic for index page which validates inputs, creates empty div, sets inner html for that div, and appends the div to dom element.

--- js/app.js
+++ js/app.js
@@ -4,32 +4,44 @@
 //set and get variables for dashboard page
 
 const getForm = document.querySelector('#etl_form1');
-const getName = document.querySelector('#name');
-const getEmail = document.querySelector('#email');
+const nameInputValue = document.querySelector('#name');
+const emailInputValue = document.querySelector('#email');
+const signInError = document.getElementById('errorMessage');
 
 //set and get error message for page (name)
 
-const errorMessName = document.querySelector('#errorName');
-const errorMessEmail = document.querySelector('#errorEmail');
+// const displayErrorNameEl = document.querySelector('#errorName');
+// const displayErrorEmailEl = document.querySelector('#errorEmail');
 
 
-//set up event listener for dashboard page
+
+//set up event listener for sign-in page
+
 
 getForm.addEventListener('submit',(evt)=>{
-
+    signInError.innerHTML = "";
     evt.preventDefault();
 
-    if(getName.value === ""){
-        const errorMess = document.createElement('div');
-        errorMess.innerHTML = "you must enter your name";
-        errorMessName.append(errorMess);
+    if(
+        checkNotEmpty(nameInputValue,"you must enter a name")&&
+        checkNotEmpty(emailInputValue,"you must enter an email")
+    ){
+        window.location.href = '/dashboard.html';
     }
 
-    if(getEmail.value === ""){
-        const errorMess = document.createElement('div');
-        errorMess.innerHTML = "you must enter your name";
-        errorMessEmail.append(errorMess);
+});
+
+//created function to validate inputs, create div,set inner html to create div, and append to element on index page.
+
+function checkNotEmpty(domInput,errorMessage){
+    if(domInput.value === ""){
+        const div = document.createElement('div');
+        div.innerHTML =`<p class="bg-blue-200 p-3 w-full rounded-lg 
+                        text-center text-blue-900 font-bold">${errorMessage}</p>`;
+        signInError.appendChild(div);
+        return false; //ends block of code in if statement
     }
 
+return true}
 
-})
+
