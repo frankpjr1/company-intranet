ff0bd48027c4ffac4ad8d9bbcfd88ac07c61f3cc
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-01-26 21:41:22 -0500

    created logic to validate input elements on the page

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,24 +4,42 @@
 //set and get variables for dashboard page
 
 const getDashboardForm = document.querySelector('#etl_form2');
-const getDashboardName = document.querySelector('#dashboard-name');
-const getDashboardStory = document.querySelector('#employee-story');
+const nameInputValue = document.querySelector('#dashboard-name');
+const getDashboardDate = document.querySelector('#dashboard-date');
+const storyInputValue = document.getElementById('employee-story');
+const signInError = document.getElementById('submitStoryError');
+
+
 
 //set and get error message for page (name)
 
-const errorMessDash = document.querySelector('#error-name-dash');
+
 
 
 //set up event listener for dashboard page
 
 getDashboardForm.addEventListener('submit',(evt)=>{
-
+    signInError.innerHTML = "";
     evt.preventDefault();
 
-    if (getDashboardStory){
-        alert("you forgot to tell us your story");
-        return false;
+    if (
+        checkNotEmpty(nameInputValue,"your name is required")&&
+        checkNotEmpty(storyInputValue,"short story required")
+    ){
+
+        }
+
+
+});
+
+
+function checkNotEmpty(domInput,errorMessage){
+    if(domInput.value === ""){
+        const div = document.createElement('div');
+        div.innerHTML =`<p class="bg-blue-200 p-3 w-full rounded-lg 
+                        text-center text-blue-900 font-bold">${errorMessage}</p>`;
+        signInError.appendChild(div);
+        return false; //ends block of code in if statement
     }
 
-
-})
+    return true}
