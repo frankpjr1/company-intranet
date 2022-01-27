64e92f5c55bdb0b6be3448dc441e3047b37a80bf
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-01-27 11:07:34 -0500

    commit logic completion

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,47 +1,42 @@
-//code for dashboard page
-// const story1 = {
-//         name:"Jordan Blake",
-//         date:"1/25/22",
-//         content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit"}
-//
-// const story2 = {
-//     name:"Sue Smith",
-//     date:"1/23/22",
-//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit"}
-//
-// const story3 = {
-//     name:"Mike harris",
-//     date:"1/21/22",
-//     content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit"}
-//
-//
-// console.log(story1)
-// console.log(story2)
-// console.log(story3)
+// code for dashboard page
+const story1 = {
+    name: "Jordan Blake",
+    date: "1/25/22",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+}
+
+const story2 = {
+    name: "Sue Smith",
+    date: "1/23/22",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+}
+
+const story3 = {
+    name: "Mike harris",
+    date: "1/21/22",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+}
+
+
+console.log(story1)
+console.log(story2)
+console.log(story3)
 
 let allNews = [{
-    name:"Jordan Blake",
-    date:"1/25/22",
-    content:"I loved the company trip to New York"},{
-    name:"Sue Smith",
-    date:"1/23/22",
-    content:"This company has provided me with a great opportunity to volunteer for a local charity"},{
-    name:"Mike harris",
-    date:"1/21/22",
-    content:"I love the city of Boston"},{
-    name:"Mike PLease",
-    date:"1/19/22",
-    content:"I love the city of Houston"},{
-    name:"Dele O",
-    date:"1/14/22",
-    content:"I love the city of Miami"}
+    name: "Jordan Blake",
+    date: "1/25/22",
+    content: "I loved the company trip to New York"
+}, {
+    name: "Sue Smith",
+    date: "1/23/22",
+    content: "This company has provided me with a great opportunity to volunteer for a local charity"
+}
 
 
 ];
 
 // displayAllNews(allNews)
 const newsStory = document.getElementById('employeeStory');
-
 
 
 //set and get variables for dashboard page
@@ -53,46 +48,41 @@ const storyInputValue = document.getElementById('employee-story');
 const signInError = document.getElementById('submitStoryError');
 
 
-
 //set and get error message for page (name)
-
-
 
 
 //set up event listener for dashboard page
 
-getDashboardForm.addEventListener('submit',(evt)=>{
-    signInError.innerHTML = "";
+getDashboardForm.addEventListener('submit', (evt) => {
+
     evt.preventDefault();
+        console.log(nameInputValue.value);
+    if(
+        checkNotEmpty(nameInputValue.value,"you must enter a name")&&
+        checkNotEmpty(storyInputValue.value.trim(),"you must enter a story")
+    ){
+        const newNews = {
+            name: document.getElementById('dashboard-name').value, date: "1/27/22",
+            content: document.getElementById('employee-story').value
+        }
 
 
-    // if (
-    //     checkNotEmpty(nameInputValue,"your name is required")&&
-    //     checkNotEmpty(storyInputValue,"short story required")
-    // ){
-    //
-    //     }
 
-   // const storyAuthor = document.getElementById('dashboard-name').value;
-   // const storyCopy = document.getElementById('employee-story').value;
-   //  console.log(document.getElementById('dashboard-name').value);
-   //  console.log(document.getElementById('employee-story').value);
-
-    //spread operator
-    const newNews = {name:document.getElementById('dashboard-name').value,date:"1/27/22",
-        content:document.getElementById('employee-story').value
+        allNews = [...allNews, newNews];
+        displayAllNews(allNews);
+        nameInputValue.value = "";
+        storyInputValue.value = "";
     }
 
-    console.log(allNews);
-    console.log(newNews);
 
-    allNews = [...allNews, newNews];
 
-    console.log(allNews);
+
+    //spread operator
+
     // console.log(storyCopy);
 });
 
-for (const key in allNews){
+for (const key in allNews) {
     const newsCardDiv = document.createElement('div');
     newsCardDiv.innerHTML = `<div>
             <h3 class="font-medium">${allNews[key].name}</h3>
@@ -102,27 +92,33 @@ for (const key in allNews){
         </div>`;
     newsStory.appendChild(newsCardDiv);
 }
-// function displayAllNews(items){
-//     for (const key in items){
-//         const newsCardDiv = document.createElement('div');
-//         newsCardDiv.innerHTML = `<div>
-//             <h3 class="font-medium">${items[key].name}</h3>
-//             <h5 class="text-sm text-yellow-200 my-2">${items[key].date}</h5>
-//             <p class="pb-2">${items[key].content}</p>
-//             <hr class="py-3">
-//         </div>`;
-//         newsStory.appendChild(newsCardDiv);
-// }
+
+function displayAllNews(items) {
+    newsStory.innerHTML = "";
+    for (const key in items) {
+        const newsCardDiv = document.createElement('div');
+        newsCardDiv.innerHTML = `<div>
+            <h3 class="font-medium">${items[key].name}</h3>
+            <h5 class="text-sm text-yellow-200 my-2">${items[key].date}</h5>
+            <p class="pb-2">${items[key].content}</p>
+            <hr class="py-3">
+        </div>`;
+        newsStory.appendChild(newsCardDiv);
+    }
 
 
 
-function checkNotEmpty(domInput,errorMessage){
-    if(domInput.value === ""){
+}
+function checkNotEmpty(domInput, errorMessage) {
+    signInError.innerHTML = "";
+    console.log("hello");
+    if (domInput === "") {
         const div = document.createElement('div');
-        div.innerHTML =`<p class="bg-blue-200 p-3 w-full rounded-lg 
+        div.innerHTML = `<p class="bg-blue-200 p-3 w-full rounded-lg 
                         text-center text-blue-900 font-bold">${errorMessage}</p>`;
         signInError.appendChild(div);
         return false; //ends block of code in if statement
     }
 
-    return true}
+    return true
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    colors:{
+      'revolver': {
+        '50': '#f5f4f5',
+        '100': '#ece8ec',
+        '200': '#cec7cf',
+        '300': '#b1a5b3',
+        '400': '#776179',
+        '500': '#3c1d40',
+        '600': '#361a3a',
+        '700': '#2d1630',
+        '800': '#241126',
+        '900': '#1d0e1f'
+      }
+    }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
